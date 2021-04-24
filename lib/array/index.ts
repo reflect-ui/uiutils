ea762a4f139a746de812c0e19a5389e2ae5e7d81
softmarshmallow <woojoo@softmarshmallow.com>
2021-04-25 02:14:08 +0900

    add array utils - "notEmpty" for general usage

--- lib/array/index.ts
+++ lib/array/index.ts
@@ -2,7 +2,9 @@ export * from "./is-all-equal";
 
 // region filters
 import { onlyUnique } from "./filter-only-unique";
+import { notEmpty } from "../validators/not-empty";
 export const filters = {
   onlyUnique,
+  notEmpty,
 };
 // endregion
