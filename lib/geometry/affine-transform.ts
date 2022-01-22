14655964ab4cf49dd4bbfc39d43aeeab4d4f9f40
softmarshmallow <woojoo@softmarshmallow.com>
2022-01-23 06:23:54 +0900

    update affine transform

--- lib/geometry/affine-transform.ts
+++ lib/geometry/affine-transform.ts
@@ -5,17 +5,13 @@
  */
 import { Point } from "../types";
 
-export interface Transformable {
-  transform(affineTransform: AffineTransform): this;
-}
-
 /**
  * For more details:
  *
  * https://en.wikipedia.org/wiki/Transformation_matrix#Affine_transformations
  * https://people.cs.clemson.edu/~dhouse/courses/401/notes/affines-matrices.pdf
  */
-export class AffineTransform implements Transformable {
+export class AffineTransform {
   m00: number;
   m01: number;
   m02: number;
@@ -23,7 +19,9 @@ export class AffineTransform implements Transformable {
   m11: number;
   m12: number;
 
-  constructor([m00, m01, m02, m10, m11, m12]: number[] = [0, 0, 0, 0, 0, 0]) {
+  constructor(
+    [m00, m01, m02, m10, m11, m12]: Iterable<number> = [0, 0, 0, 0, 0, 0]
+  ) {
     this.m00 = m00;
     this.m01 = m01;
     this.m02 = m02;
@@ -45,7 +43,7 @@ export class AffineTransform implements Transformable {
     };
   }
 
-  transform(other: AffineTransform): this {
+  prepend(other: AffineTransform): AffineTransform {
     return new AffineTransform([
       this.m00 * other.m00 + this.m01 * other.m10, // m00
       this.m00 * other.m01 + this.m01 * other.m11, // m01
@@ -53,7 +51,18 @@ export class AffineTransform implements Transformable {
       this.m10 * other.m00 + this.m11 * other.m10, // m10
       this.m10 * other.m01 + this.m11 * other.m11, // m11
       this.m10 * other.m02 + this.m11 * other.m12 + this.m12, // m12
-    ]) as this;
+    ]);
+  }
+
+  append(other: AffineTransform): AffineTransform {
+    return new AffineTransform([
+      other.m00 * this.m00 + other.m01 * this.m10, // m00
+      other.m00 * this.m01 + other.m01 * this.m11, // m01
+      other.m00 * this.m02 + other.m01 * this.m12 + other.m02, // m02
+      other.m10 * this.m00 + other.m11 * this.m10, // m10
+      other.m10 * this.m01 + other.m11 * this.m11, // m11
+      other.m10 * this.m02 + other.m11 * this.m12 + other.m12, // m12
+    ]);
   }
 
   get determinant() {
@@ -105,11 +114,7 @@ export class AffineTransform implements Transformable {
     return new AffineTransform([1, 0, 0, 0, 1, 0]);
   }
 
-  static rotation(
-    theta: number,
-    rx: number = 0,
-    ry: number = 0
-  ): AffineTransform {
+  static rotate(theta: number, origin?: Point): AffineTransform {
     const rotation = new AffineTransform([
       Math.cos(theta),
       -Math.sin(theta),
@@ -119,24 +124,50 @@ export class AffineTransform implements Transformable {
       0,
     ]);
 
-    if (rx !== 0 || ry !== 0) {
+    if (origin && (origin.x !== 0 || origin.y !== 0)) {
       return AffineTransform.multiply(
-        AffineTransform.translation(rx, ry),
+        AffineTransform.translate(origin.x, origin.y),
         rotation,
-        AffineTransform.translation(-rx, -ry)
+        AffineTransform.translate(-origin.x, -origin.y)
       );
     }
 
     return rotation;
   }
 
-  static scale(sx: number, sy: number = sx): AffineTransform {
-    return new AffineTransform([sx, 0, 0, 0, sy, 0]);
+  rotate: typeof AffineTransform["rotate"] = (...args) => {
+    return this.prepend(AffineTransform.rotate(...args));
+  };
+
+  static scale(
+    sx: number,
+    sy: number = sx,
+    origin: Point = { x: 0, y: 0 }
+  ): AffineTransform {
+    const scale = new AffineTransform([sx, 0, 0, 0, sy, 0]);
+
+    if (origin.x !== 0 || origin.y !== 0) {
+      return AffineTransform.multiply(
+        AffineTransform.translate(origin.x, origin.y),
+        scale,
+        AffineTransform.translate(-origin.x, -origin.y)
+      );
+    }
+
+    return scale;
   }
 
-  static translation(tx: number, ty: number): AffineTransform {
+  scale: typeof AffineTransform["scale"] = (...args) => {
+    return this.prepend(AffineTransform.scale(...args));
+  };
+
+  static translate(tx: number, ty: number): AffineTransform {
     return new AffineTransform([1, 0, tx, 0, 1, ty]);
   }
+
+  translate: typeof AffineTransform["translate"] = (...args) => {
+    return this.prepend(AffineTransform.translate(...args));
+  };
 
   /**
    * Multiply the matrices together from left to right.
@@ -144,6 +175,6 @@ export class AffineTransform implements Transformable {
   static multiply(...[first, ...rest]: AffineTransform[]): AffineTransform {
     if (!first) return AffineTransform.identity;
 
-    return rest.reduce((result, item) => result.transform(item), first);
+    return rest.reduce((result, item) => result.prepend(item), first);
   }
 }
